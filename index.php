57ddbf75658943a0fa9ba4fdf1d5a2ee80f82043
AdaM2196 <38299774+AdaM2196@users.noreply.github.com>
2018-04-11 22:32:22 +0100

    HNG Internship stage one task HTML page

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Lazy Writer Landing Page</title>
+    <link href="assets/stylesheet.css" type="text/css" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700i" rel="stylesheet">
+</head>
+<body>
+    <nav class="row">
+        <p class="col-2 time"><b><i><?php date_default_timezone_set("Africa/Lagos"); echo date("h:i A")?></i></b></p>
+        <ul class="col-10 menu-items">
+            <li><a href= "#">Home</a></li>
+            <li><a href="#">About Me</a></li>
+            <li><a href="#">Contact Me</a></li>
+        </ul>
+    </nav>
+    <div class="body-text">
+        <h1><b><i>Welcome <br> to <br/> The Lazy Writer Blog</i></b> </h1>
+    </div>
+    
+</body>
+</html>
